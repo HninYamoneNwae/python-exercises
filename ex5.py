a0f07bead22994a27f49a1fd81bdf77d7f46c5e1
HninYamoneNwae <hninyamonenway@gmail.com>
2018-06-05 21:32:19 +0630

    My 5th python exercise

---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name='Hymn'
+my_age=21
+my_height=70
+my_weight=180
+my_eyes='Black'
+my_teeth='white'
+my_hair='Brown'
+print ("Let's talk about %s."%my_name)
+print ("She's %d inches tall."%my_height)
+print ("She's %d pounds heavy."%my_weight)
+print ("Actually that's not too heavy.")
+print ("she's got %s eyes and %s hair."%(my_eyes,my_hair))
+print ("Her teeth are usually %s depending on the coffee."%my_teeth)
+print ("If I add %d,%d and %d I get %d."%(my_age,my_height,my_weight,my_age+my_height+my_weight))
+
